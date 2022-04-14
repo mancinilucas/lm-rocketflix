a5442e4e5bb2c633dfbb9930a96a0d2089888070
FARFETCH\luciano.lima <luciano.lima@farfetch.com>
2022-04-14 11:42:00 +0100

    feat(tweaks): There ya go
    Added vite
    Add .gitignore
    Separate logic into functions
    Added tap to better debug
    May delete everyhing if not necessary

--- main.js
+++ main.js
@@ -1,38 +1,62 @@
-import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+
+function createMovieInfo({
+  poster_path,
+  title = "<p>Ops, esse filme está indisponível no momento.</p>",
+  overview = "",
+} = {}) {
+  const poster = poster_path
+    ? IMG_URL + poster_path
+    : "https://i.pinimg.com/564x/99/a9/33/99a93345cee50bfd2a3bc62c7a23a225.jpg";
+
+  return `<div class="movie-info">
+  <img src="${poster}">
+  <div>
+    <h2>${title}</h2>
+    <p>
+      ${overview}
+    </p>
+  </div>
+ </div>`;
+}
+
+function appendToMovie(content) {
+  const movie = document.getElementById("movie");
+  movie && (movie.innerHTML = content);
+}
+
+function tap(fn) {
+  return function (x) {
+    fn(x);
+    return x;
+  };
+}
 
 function getMovieInfo() {
-  const id = Math.floor(Math.random() * 1000) + 1
+  const id = Math.floor(Math.random() * 1000) + 1;
 
-  const movie_url = `${BASE_URL}${id}?${API_KEY}&${language}`
+  const movie_url = `${BASE_URL}${id}?${API_KEY}&${language}`;
+
   fetch(movie_url)
-    .then(response => response.json())
-    .then(data => {
-      movie.innerHTML = `
-       <div class="movie-info">
-        <img src="${
-          data.poster_path
-            ? IMG_URL + data.poster_path
-            : 'https://i.pinimg.com/564x/99/a9/33/99a93345cee50bfd2a3bc62c7a23a225.jpg'
-        }">
-        <div>
-          <h2>${
-            data.title
-              ? data.title
-              : '<p>Ops, esse filme está indisponível no momento.</p>'
-          }</h2>
-          <p>
-            ${data.overview ? data.overview : ''}
-          </p>
-        </div>
-       </div>
-       `
-    })
+    .then((response) => response.json())
+    // Get only what you need
+    .then(({ poster_path, title, overview }) => ({
+      poster_path,
+      title,
+      overview,
+    }))
+    // Caso queira debugar a "pipe" das promises... basta dar um tap...
+    // .then(tap(console.log))
+    // Create the HTML representation
+    .then(createMovieInfo)
+    // Then append it
+    .then(appendToMovie);
 }
-
-const buttonMovie = document.getElementById('buttonMovie')
 
 function changeMovie() {
-  getMovieInfo()
+  getMovieInfo();
 }
 
-buttonMovie.addEventListener('click', changeMovie)
+window.app = {
+  changeMovie,
+};
